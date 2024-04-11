5548c65e38a579724b5b1335bdda78a1aeaf6e20
Rawlings_10 <150746787+Rawlings10@users.noreply.github.com>
2024-04-11 19:56:01 -0400

    Add files via upload

---
+++ NarrativeProject/Rooms/Bedroom.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NarrativeProject.Rooms
+{
+    internal class Bedroom : Room 
+    {
+
+        internal override string CreateDescription() =>
+@"You find yourself in a room your hands are tied together.
+And you  are seeing walls of blood and strange objects.
+The room creates a disturbing and gloomy atmosphere of mental desolation. 
+You need to find a way to loosen your hands. 
+
+go [left] or [right] of the room!!!
+
+.";
+
+
+        internal override void ReceiveChoice(string choice)
+        {
+            switch (choice)
+            {
+                case "left":
+                    Console.WriteLine("You enter the bathroom.");
+                    Game.Transition<Bathroom>();
+                    break;
+                case "right":
+                    if (!AtticRoom.isKeyCollected)
+                    {
+                        Console.WriteLine("The door is locked."); 
+                    }
+                    else
+                    {
+                        Console.WriteLine("You open the door with the key and leave your bedroom.");
+                        Game.Finish();
+                    }
+                    break;
+                case "attic":
+                    Console.WriteLine("You go up and enter your attic.");
+                    Game.Transition<AtticRoom>();
+                    break;
+                default:
+                    Console.WriteLine("Invalid command.");
+                    break;
+            }
+        }
+    }
+}
---
+++ NarrativeProject/Rooms/Start_Page.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeProject.Rooms
+{
+    internal class Start_Page : Room, IDetails
+    {
+        public void title()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                        THE HUNTED MAISON                      ");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("press any key to continue!!!");
+            Console.ReadKey();
+            Console.Beep();
+            Console.Clear();
+        }
+        public void Player_Details()
+        {
+            Console.WriteLine("Please Enter your name to start Game.");
+            Game.name = Console.ReadLine();
+            Console.WriteLine($"Welcome {Game.name} press");
+        }
+        internal override string CreateDescription() =>
+        @"You wake up in a mysterious mansion with no memory of how you got there.
+As you explore, you realize the mansion is haunted.
+Your goal is to find a way to escape before it's too late.
+
+
+
+Type [start] to proceed to game
+Type [rules] for how to play";
+        
+        internal override void ReceiveChoice(string choice)
+        {
+            
+            switch (choice)
+            {
+                case "start":
+                     Game.Transition<Bedroom>();
+                     break;
+                case "rules":
+                     Console.WriteLine("this is the rules.............");
+                     break;
+                default: 
+                     Console.WriteLine("invalid input!!!");
+                     break;
+            }
+            
+        }
+    }
+
+}
