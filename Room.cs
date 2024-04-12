a89f36fd09d90f963c59d175b3929a25361d9f61
Rawlings_10 <rawllingsoduwa@gmail.com>
2024-04-11 20:23:20 -0400

    the new repo for Rawlings and Jorge

---
+++ Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NarrativeProject
+{
+    internal abstract class Room
+    {
+        internal abstract string CreateDescription();
+        internal abstract void ReceiveChoice(string choice);
+    }
+    internal interface IDetails
+    {
+        void title();
+        void Player_Details();
+    }
+}
