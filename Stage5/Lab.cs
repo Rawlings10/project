39032dd7e58cd48a9d996c6485ad3db076516dc4
Rawlings_10 <rawllingsoduwa@gmail.com>
2024-04-13 15:37:14 -0400

    stage 5
    rooms added but will still worked on, probably more room will be added

---
+++ Stage5/Lab.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeProject.Rooms
+{
+    internal class Lab : Room
+    {
+        public Lab() { }
+
+        internal override string CreateDescription()
+        {
+            throw new NotImplementedException();
+        }
+        internal override void ReceiveChoice(string choice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
