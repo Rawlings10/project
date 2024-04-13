cd2b60e214af67b337d6faba2a2d3fc22ad5d06c
Rawlings_10 <rawllingsoduwa@gmail.com>
2024-04-13 15:36:26 -0400

    stage 1
    rooms added but will still worked on, probably more room will be added

---
+++ Stage1/Basement.cs
@@ -0,0 +1,18 @@
+﻿using NarrativeProject.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeProject.Stage1
+{
+    internal class Basement : Bedroom
+    {
+        static void Description()
+        {
+            Console.WriteLine("sgssg");
+        }
+
+    }
+}
