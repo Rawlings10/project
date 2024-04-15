c4f68e6fef4a2ee6a6b0e92bcbf6b6309d16e52e
Rawlings_10 <rawllingsoduwa@gmail.com>
2024-04-15 14:27:01 -0400

    stage 2
    rooms for stage 2

---
+++ Stage2/Door1.cs
@@ -0,0 +1,36 @@
+﻿using NarrativeProject.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeProject.Stage2
+{
+    internal class Door1 : Room
+    {
+        internal override string CreateDescription() =>
+@"You Entered the first door. 
+you entered a desert and got beaten by a snake.
+you need to kill the snake!!!!
+MAP
+backward to the corridor
+        
+         [backward]
+.";
+        
+        internal override void ReceiveChoice(string choice)
+        {
+            switch (choice)
+            {
+                case "backward":
+                    Console.WriteLine("You return to the corridor");
+                    Game.Transition<Corridor>();
+                    break;
+                default:
+                    Console.WriteLine("Invalid command.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Stage2/Door2.cs
@@ -0,0 +1,52 @@
+﻿using NarrativeProject.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeProject.Stage2
+{
+    internal class Door2 : Room
+    {
+        internal override string CreateDescription() =>
+@"You Entered the basement. 
+it is dark, quiet and cold.
+MAP
+The door forward is locked
+The door backward leads to the bedroom. 
+.
+
+          [forward]
+
+         [backward]
+.";
+        internal override void ReceiveChoice(string choice)
+        {
+            switch (choice)
+            {
+                case "forward":
+                    Console.WriteLine("The Door is look!!!");
+                    Console.WriteLine("What is the keycode???");
+                    string key = Console.ReadLine();
+                    if (key != "salut")
+                    {
+                        Console.WriteLine("The door is locked.");
+                    }
+                    else if (key == "salut")
+                    {
+                        Console.WriteLine("You open the door with the key and leave your bedroom.");
+                        Game.Transition<Corridor>();
+                    }
+                    break;
+                case "backward":
+                    Console.WriteLine("You entered the Bedroom");
+                    Game.Transition<Bedroom>();
+                    break;
+                default:
+                    Console.WriteLine("Invalid command.");
+                    break;
+            }
+        }
+    }
+}
