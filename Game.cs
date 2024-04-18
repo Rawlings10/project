81a2265e6ce480e06b4f7b59637a2b8b358ed067
Rawlings_10 <rawllingsoduwa@gmail.com>
2024-04-18 17:29:11 -0400

    code for player inventory

--- Game.cs
+++ Game.cs
@@ -1,68 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Timers;
+using System.Threading;
 using System.ComponentModel;
 using System.Media;
 using System.Security.Policy;
+using System.Runtime.InteropServices;
 
 namespace NarrativeProject
 {
     public enum gameArtifact
     {
         knife,
-        key
+        key,
     }
-    
 
     public class Game
     {
-        static List<gameArtifact> artifacts = new List<gameArtifact>();
-        
-        internal static void Inventory(gameArtifact artifact) 
-        {
-            artifacts.Add(artifact);
-            Console.WriteLine($"You picked up the {artifact}");
+        public static List<gameArtifact> Inventory { get; private set; }
 
-            foreach(gameArtifact item in artifacts) 
+        public Game()
+        {
+            Inventory = new List<gameArtifact>();
+        }
+
+        public static void AddtoInventory(gameArtifact item) 
+        {
+            Inventory.Add(item);
+            Console.WriteLine($"{item} added to found items");
+            SetTimer(1000);
+        }
+        public static void CheckInventory()
+        {
+            Console.WriteLine("INVENTORY");
+            foreach (var item in Inventory)
             {
                 Console.WriteLine(item);
             }
+            Game.SetTimer(1000);
         }
-        
+
         List<Room> rooms = new List<Room>();
         Room currentRoom;
         internal bool IsGameOver() => isFinished;
         static bool isFinished;
         static string nextRoom = "";
-        public static int HP = 100;
+        public static int PlayerHP = 100;
         public static string name;
-        public static int score;
-        public static Timer timer;
+        public static int amorPower = 3;
+        public static int Ammunation = 20;
+        public static int enermyHp = 15;
+        internal static int enemyAttackDamage = 5; 
+        public static int PlayerAttackDamage = 5;
+        public static bool playerTurn = true;
+        public static bool isAmorUsed;
           
         public static int GetHp()
         {
-            return HP;
+            return PlayerHP;
         }
         public static void SetHp(int newHP) 
         {
-            HP = newHP; 
+            PlayerHP = newHP; 
         }
         public static void PlayerDamage(int damage)
-        {          
-            SetHp(HP - damage);
+        {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"You took a damage of: {damage}");
-            if (HP <= 0)
+            Game.SetTimer(1000);
+            SetHp(PlayerHP - damage);
+            if (PlayerHP <= 0)
             {
                 Game.Finish();
             }
         }
-
-        public static void SetTimer(int miliseconds) 
+        public static int GetAmmunation() 
         {
-            timer = new Timer(miliseconds);
-            timer.Start();
-            
+            return Ammunation;
+        }
+        public static void SetAmmunation(int amunation) 
+        {
+            Ammunation = amunation;
+        }
+        public static void AmmunationHP(int bullet)
+        {
+            Ammunation += bullet;
+        }
+
+        public static int GetAmor()
+        {
+            return amorPower;
+        }
+        public static void SetAmor(int amor)
+        {
+            amorPower = amor;
+        }
+        public static void PlayerAttack()
+        {
+            Console.WriteLine("Attack the Enemy");
+            if (Ammunation > 0)
+            {
+                Ammunation--;
+                enermyHp -= PlayerAttackDamage;
+                Console.WriteLine("Good Attack");
+            }
+            else if (Ammunation <= 0)
+            {           
+                Console.WriteLine("OPPS, out of ammunation!!!");
+            }
+        }
+        public static void EnemyAttack()
+        {
+            if (isAmorUsed == true)
+            {
+                PlayerHP -= 0;
+                Console.WriteLine("No damage collected");
+            }
+            else
+            {
+                PlayerHP -= enemyAttackDamage;
+                Console.WriteLine($"Enemy Attacted, HP: {PlayerHP}");
+            }
+        }
+        public static void FightScene()
+        {
+            while(PlayerHP > 0 && enermyHp > 0 );
+            if (playerTurn)
+            {
+                Console.WriteLine($"{Game.name}'s, turn");
+                PlayerAttack();
+            }
+            else
+            {
+                Console.WriteLine("Enemy's turn");
+                EnemyAttack();
+            }
+        }
+        public static void SetTimer(int time)
+        {
+            Thread.Sleep(time);
         }
 
         internal void Add(Room room)
