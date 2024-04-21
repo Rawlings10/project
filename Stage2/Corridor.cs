c5c265c15b1b751cc1a0cb0a2d5a48a8669f3027
Rawlings_10 <rawllingsoduwa@gmail.com>
2024-04-21 11:51:33 -0400

    stage 2 complete

--- Stage2/Corridor.cs
+++ Stage2/Corridor.cs
@@ -8,34 +8,31 @@ namespace NarrativeProject.Rooms
         private int damage;
         
         internal override string CreateDescription() =>
-@"You are in a corridor that is having three doors,
+$@"{Game.name}, you are in a Corridor with three different doors.     [STAGE 2]
 Only one door is the right door, 
-find the right door else you would get hurt when you enter the wrong room.
+
 MAP
-[left] door1
-[right] door2
-[forward] door3
-[backward] bedroom
-";
+go left - door 1
+go right -door 3
+go forward - door 2
+go backward - bedroom";
 
         internal override void PlayerMove(ConsoleKey key)
         {
             switch (key)
             {
                 case ConsoleKey.UpArrow:
-                    Console.WriteLine("You open the third door.");
-                    Game.Transition<Door3>();
-                    Game.PlayerDamage(15);
-                    break;
-                case ConsoleKey.LeftArrow:
                     Console.WriteLine("You open the second door.");
                     Game.Transition<Door2>();
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Console.WriteLine("You open the third door.");
+                    Game.Transition<Door1>();
                     
                     break;
                 case ConsoleKey.RightArrow:
                     Console.WriteLine("You open the first door");
-                    Game.PlayerDamage(10);
-                    Game.Transition<Door1>();
+                    Game.Transition<Door3>();
                     break;
                 case ConsoleKey.DownArrow:
                     Console.WriteLine("You return to the bedroom.");
@@ -43,6 +40,7 @@ MAP
                     break;
                 case ConsoleKey.Tab:
                     Game.CheckInventory();
+                    Game.SetTimer(1000);
                     break;
                 default:
                     Console.WriteLine("Invalid command.");
--- Stage2/Door2.cs
+++ Stage2/Door2.cs
@@ -11,22 +11,17 @@ namespace NarrativeProject.Stage2
     internal class Door2 : Room
     {
         internal override string CreateDescription() =>
-@"You Opened the right door. 
-this room has one door straight that takes you to the next stage.
-but note the door is ilusional, it take can you to different places.
-but you have to find you way back and keep trying until you enter the next stage.
+$@"{Game.name}, you Entered the second door.     [STAGE 2]
+
 MAP
-go forward
- 
-          [forward]
-.";
+Backward leads back to the corridor.";
         internal override void PlayerMove(ConsoleKey key)
         {
             switch (key)
             {
                 case ConsoleKey.UpArrow:
                     Random ilussion = new Random();
-                    int ilussionalDoor = ilussion.Next(0, 5);
+                    int ilussionalDoor = ilussion.Next(1, 4);
                     if (ilussionalDoor == 0)
                     {
                         Console.WriteLine("You find yourself in the Basement.");
@@ -34,8 +29,8 @@ go forward
                     }
                     else if (ilussionalDoor == 1)
                     {
-                        Console.WriteLine("You find yourself in the Bathroom.");
-                        Game.Transition<Bathroom>();
+                        Console.WriteLine("You find yourself in the Bedroom.");
+                        Game.Transition<Bedroom>();
                     }
                     else if (ilussionalDoor == 2)
                     {
@@ -44,18 +39,23 @@ go forward
                     }
                     else if (ilussionalDoor == 3)
                     {
-                        Console.WriteLine("You find yourself in the Corridor.");
-                        Game.Transition<Door2>();
-                    }
-                    else if (ilussionalDoor == 4)
-                    {
-                        Console.WriteLine("You find yourself in the Corridor.");
+                        Console.WriteLine("You passed the Portal.");
                         Game.Transition<Room3>();
                     }
                     break;
+                case ConsoleKey.RightArrow:
+                    Game.EmptySpace();
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Game.AmmunationHP();
+                    break;
                 case ConsoleKey.DownArrow:
-                    Console.WriteLine("You entered the Bedroom");
-                    Game.Transition<Bedroom>();
+                    Console.WriteLine("You back to the corridor");
+                    Game.Transition<Corridor>();
+                    break;
+                case ConsoleKey.Tab:
+                    Game.CheckInventory();
+                    Game.SetTimer(1000);
                     break;
                 default:
                     Console.WriteLine("Invalid command.");
